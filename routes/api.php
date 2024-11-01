00486ccc99b25a6d219801728c8f73995e15a889
aspire <web_on_era@hotmail.com>
2024-11-01 08:16:44 -0600

    master, mostrando informacion de api al llegar a /

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTAuthController;
 use App\Http\Middleware\JwtMiddleware;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Bienvenido a la API de Bakery',
+        'version' => '1.0',
+        'documentation' => url('/api/documentation') // Cambia esto si tienes documentación
+    ]);
+});
+
 Route::post('register', [JWTAuthController::class, 'register']);
 Route::post('login', [JWTAuthController::class, 'login']);
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/api'); // Redirige a la raíz de la API
 });
